81ec187d06f71ae79c9deb0250f69fa851615d53
cmskorcz <chaseskorey@icloud.com>
2022-07-10 14:26:00 -0400

    working on handlebars layouts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,98 @@
 const router = require('express').Router()
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
-router.get('/', (req, res) => {
-    res.render('homepage');
+router.get('/', async (req, res) => {
+    try {
+        const postsArr = await Post.findAll({
+            attributes: [
+                'id',
+                'title',
+                'post_content',
+                'created_at'
+            ],
+            include: [
+                {
+                    model: Comment,
+                    attributes: [
+                        'id',
+                        'comment_content',
+                        'post_id',
+                        'user_id',
+                        'created_at'
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+        const posts = postsArr.map(post => post.get({ plain: true }));
+        const isLoggedIn = req.session.isLoggedIn;
+        res.render('homepage', { posts, isLoggedIn });        
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.get('/login', async (req, res) => {
+    if (req.session.isLoggedIn) {
+        res.redirect('/')
+        return;
+    }
+    res.render('login')
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const foundPost = await Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'post_content',
+                'title',
+                'created_at'
+            ],
+            include: [
+                {
+                    model: Comment,
+                    attributes: [
+                        'id',
+                        'comment_content',
+                        'post_id',
+                        'user_id',
+                        'created_at'
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+    
+        if (!post) {
+            res.status(404).json({ message: 'Unable to find post' });
+            return;
+        }
+        const post = foundPost.get({ plain: true });
+        const isLoggedIn = req.session.isLoggedIn;
+    
+        res.render('single-post', { post, isLoggedIn });    
+    } catch (error) {
+        res.status(500).json(error);   
+    }
 })
 
 module.exports = router;
